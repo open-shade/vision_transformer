a9f6ec8655d895463cafb3b4156889fa3d9c0e1f
Brandon Fan <brandonfan1256@gmail.com>
2022-07-08 16:06:27 -0400

    update cloud build for vit

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,7 +1,8 @@
 declare -a ROS_VERSIONS=( "foxy" "galactic" "humble" "rolling" )
 
 ORGANIZATION="google"
-declare -a MODEL_VERSIONS=( "vit-base-patch16-224" "vit-base-patch16-384" "vit-base-patch32-384" "vit-large-patch16-224" "vit-large-patch16-384" )
+MODEL_NAME="vit"
+declare -a MODEL_VERSIONS=( "base-patch16-224" "base-patch16-384" "base-patch32-384" "large-patch16-224" "large-patch16-384" )
 
 
 for VERSION in "${ROS_VERSIONS[@]}"
@@ -9,7 +10,9 @@ do
   for MODEL_VERSION in "${MODEL_VERSIONS[@]}"
   do
     ROS_VERSION="$VERSION"
-    gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_MODEL_VERSION="$MODEL_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
+    HF_VERSION="$MODEL_NAME-$MODEL_VERSION"
+    TAG="$MODEL_VERSION"
+    gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_TAG="$TAG",_MODEL_VERSION="$HF_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
     pids+=($!)
     echo Dispatched "$MODEL_VERSION" on ROS "$ROS_VERSION"
   done
